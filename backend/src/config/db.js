e995a35bcf69f629933de70b9177caa65781371a
mashrapovmuhammadrizo371-sudo <mashrapovmuhammadrizo371@gmail.com>
2026-09-06 18:57:59 +0500

    Create db.js

---
+++ backend/src/config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+  const uri = process.env.MONGO_URI;
+  if (!uri) {
+    throw new Error('MONGO_URI is not set in environment variables');
+  }
+
+  mongoose.set('strictQuery', true);
+
+  try {
+    const conn = await mongoose.connect(uri);
+    console.log(`[db] MongoDB connected: ${conn.connection.host}/${conn.connection.name}`);
+  } catch (err) {
+    console.error('[db] MongoDB connection failed:', err.message);
+    process.exit(1);
+  }
+
+  mongoose.connection.on('disconnected', () => {
+    console.warn('[db] MongoDB disconnected');
+  });
+}
+
+module.exports = connectDB;
