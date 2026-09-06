18962473dab4717fd1d4008f41a26d125af45bff
mashrapovmuhammadrizo371-sudo <mashrapovmuhammadrizo371@gmail.com>
2026-09-06 19:05:03 +0500

    Create permissions.js

---
+++ backend/src/config/permissions.js
@@ -0,0 +1,43 @@
+const PERMISSIONS = {
+  DASHBOARD_VIEW: 'dashboard.view',
+
+  BRANDS_VIEW: 'brands.view',
+  BRANDS_CREATE: 'brands.create',
+  BRANDS_UPDATE: 'brands.update',
+  BRANDS_DELETE: 'brands.delete',
+
+  MENU_VIEW: 'menu.view',
+  MENU_CREATE: 'menu.create',
+  MENU_UPDATE: 'menu.update',
+  MENU_DELETE: 'menu.delete',
+
+  BANNERS_VIEW: 'banners.view',
+  BANNERS_CREATE: 'banners.create',
+  BANNERS_UPDATE: 'banners.update',
+  BANNERS_DELETE: 'banners.delete',
+
+  TABLES_VIEW: 'tables.view',
+  TABLES_CREATE: 'tables.create',
+  TABLES_UPDATE: 'tables.update',
+  TABLES_DELETE: 'tables.delete',
+
+  ORDERS_VIEW: 'orders.view',
+  ORDERS_UPDATE: 'orders.update',
+
+  CUSTOMERS_VIEW: 'customers.view',
+
+  PROMOCODES_VIEW: 'promocodes.view',
+  PROMOCODES_CREATE: 'promocodes.create',
+  PROMOCODES_UPDATE: 'promocodes.update',
+  PROMOCODES_DELETE: 'promocodes.delete',
+
+  EMPLOYEES_VIEW: 'employees.view',
+  EMPLOYEES_CREATE: 'employees.create',
+  EMPLOYEES_UPDATE: 'employees.update',
+  EMPLOYEES_DELETE: 'employees.delete',
+
+  SETTINGS_VIEW: 'settings.view',
+  SETTINGS_UPDATE: 'settings.update'
+};
+
+module.exports = PERMISSIONS;
